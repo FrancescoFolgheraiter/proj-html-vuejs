9f5e9722334559bd5d6050bedbc783db77e4aca9
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-01-29 17:41:17 +0100

    update index

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,7 +16,22 @@ export default{
 </script>
 
 <template>
-home
+	<section>
+		<div class="container">
+			<h2>
+				Welcome to DogMilo Pets
+			</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, totam?
+			</p>
+			<button>
+				Get Started
+			</button>
+			<button>
+				Play
+			</button>
+		</div>
+	</section>
 </template>
 
 <style lang ="scss" scoped>

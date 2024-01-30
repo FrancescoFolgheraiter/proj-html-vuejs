758ee32713d9d0df8166a64db66d41d9cbda95c9
PaoloTarcisio <paoloc50@gmail.com>
2024-01-30 20:07:40 +0100

    Popolamento oggetti in AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,40 @@
 export default{
 	data() {
 		return{
-			text:"Footer"
+			lists: [
+				{
+					title: "Support",
+					links: [
+						"Contact Us",
+						"Online Chat",
+						"Whatsapp",
+						"Telegram",
+						"Snapchat",
+					]
+				},
+				{
+					title: "About",
+					links: [
+						"About Us",
+						"Blog",
+						"Career",
+						"Job",
+						"In Press",
+					]
+				},
+				{
+					title: "Faq",
+					links: [
+						"Account",
+						"Order",
+						"Delivery",
+						"Payments",
+						"Returns",
+					]
+				}
+			],
+			Socials: [],
+			Contacts:[]
 		};
 	},
 	components:{
@@ -17,7 +50,36 @@ export default{
 
 <template>
 	<footer>
-		{{ text }}
+		<!-- <section class="footer-newsletter">
+			<div class="newsletter">
+				<div class="img-newsletter">
+					<img src="/img/news.png" alt="news.png">
+				</div>
+				<div class="sub-newsletter">
+					<h2>Subscribe to Our Newsletter</h2>
+					<form action="">
+						<div class="input-group mb-3">
+							<input type="text" class="form-control" placeholder="Email" aria-label="Recipient's username" aria-describedby="basic-addon2">
+							<span class="input-group-text" id="basic-addon2">@</span>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
+		<section class="links-and-contacts">
+			<ul>
+				<li v-for="(listItem, i) in lists">
+					<ul>{{ listItem.title }}
+						<li v-for = "(link, j) in lists.length">
+							{{ listItem.links[j] }}
+						</li>
+					</ul>
+					
+				</li>
+			</ul>
+		</section> -->
+
+		footer
 	</footer>
 </template>
 

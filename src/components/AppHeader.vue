2f213fe91c796ff54b6259477c66b445306edcf3
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-01-30 11:44:48 +0100

    Added header and images

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,10 +17,200 @@ export default{
 
 <template>
 	<header>
-		{{ text }}
+		<section class="info-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-3">
+						<i class="fa-solid fa-envelope"></i>
+						info@example.com
+					</div>
+					<div class="col">
+						<i class="fa-solid fa-phone"></i>
+						+0 123-456-7890
+					</div>
+					<div class="col socials">
+						<div class="facebook">
+							<i class="fa-brands fa-facebook"></i>
+						</div>
+						<div class="twitter">
+							<i class="fa-brands fa-twitter"></i>
+						</div>
+						<div class="instagram">
+							<i class="fa-brands fa-instagram"></i>
+						</div>
+						<div class="linkedin">
+							<i class="fa-brands fa-linkedin"></i>
+						</div>
+						<div class="pinterest">
+							<i class="fa-brands fa-pinterest"></i>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="main-header">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-3 logo-container">
+						<img src="/img/logo.png" alt="">
+					</div>
+
+					<div class="col-9">
+
+						<nav class="navbar navbar-expand-lg">
+						
+							<div id="navbarNavDropdown">
+								<ul class="navbar-nav">
+									<li class="nav-item dropdown">
+										<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+											Home
+										</a>
+										<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+											<a class="dropdown-item" href="#">Home One</a>
+											<a class="dropdown-item" href="#">Home Two</a>
+											<a class="dropdown-item" href="#">Home Three</a>
+										</div>
+									</li>
+
+									<li class="nav-item dropdown">
+										<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+											About
+										</a>
+										<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+											<a class="dropdown-item" href="#">About One</a>
+											<a class="dropdown-item" href="#">About Two</a>
+										</div>
+									</li>
+
+									<li class="nav-item active">
+										<a class="nav-link" href="#">Services <span class="sr-only">(current)</span></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Training</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Blogs</a>
+									</li>
+
+									<button class="rounded-pill">
+										Get In Touch
+									</button>
+
+								
+								</ul>
+							</div>
+						</nav>
+					</div>
+
+				</div>
+			</div>
+		</section>
+			
+
+			
 	</header>
 </template>
 
 <style lang ="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+
+.info-header {
+	background-color: $main-color;
+	height: 50px;
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	color: white;
+
+}
+.fa-envelope,
+.fa-phone {
+	color: $second-color;
+	font-size: 1.3em;
+}
+.socials {
+	width: auto;
+	display: flex;
+	justify-content: flex-end;
+
+
+	>div {
+		display: flex;
+		flex-wrap: wrap;
+		width: 30px;
+		height: 30px;
+		border-radius: 10%;
+		margin: 0 3px;
+		justify-content: center;
+		align-content: center;
+
+		>i {
+			color: white;
+		}
+	}
+}
+.facebook {
+	background-color: #3744B3;
+}
+
+.twitter {
+	background-color: #7CC6C5;
+}
+
+.instagram {
+	background-color: #A329A8;	
+}
+.linkedin {
+	background-color: #1164C2;
+}
+.pinterest {
+	background-color: #B61220;
+}
+.main-header {	
+	height: 80px;
+	display: flex;	
+	flex-wrap: wrap;
+	align-items: center;
+}
+.logo-container {
+	align-self: center;
+}
+button {
+	background-color: $second-color;
+	border: none;
+	border-radius: 5%;
+	color: white;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 1em;
+	font-weight: bold;
+}
+.col-9 {
+	justify-content: space-between;
+	align-items: center;
+}
+.navbar-expand-lg {
+    flex-wrap: wrap;
+    justify-content: center;
+	margin: 0 20px;
+	font-weight: 600;
+	font-size: larger;
+	color: $text-color;
+		
+		:not(button):hover {
+			color: $second-color
+		}
+
+}
+.navbar-nav {
+	display: flex;
+    justify-content: space-evenly;
+    align-items: baseline;
+}
 
 </style>
